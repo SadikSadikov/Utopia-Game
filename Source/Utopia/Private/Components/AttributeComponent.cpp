c8d8ccd8520aafdd7d95d3837c2a3988497a32f5
Sadik Sadikov <sadikscorpion@gmail.com>
2024-10-24 18:02:55 +0300

    Attribute Component

---
+++ Source/Utopia/Private/Components/AttributeComponent.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AttributeComponent.h"
+
+
+UAttributeComponent::UAttributeComponent()
+{
+
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
+
+
+void UAttributeComponent::TakeDamage(float DamageAmount)
+{
+	Health = FMath::Clamp(Health - DamageAmount, 0.f, MaxHealth);
+
+	OnDamageTakenDelegate.Broadcast();
+
+	if (!IsAlive())
+	{
+		OnDeahtDelegate.Broadcast();
+	}
+}
+
+void UAttributeComponent::RegenHealth(float DeltaTime)
+{
+	Health = FMath::Clamp(Health + HealthRegenRate * DeltaTime, 0.f, MaxHealth);
+}
+
+bool UAttributeComponent::IsAlive()
+{
+	return Health > 0.f;
+}
+
+void UAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	
+}
+
+
+
+void UAttributeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
+}
+
---
+++ Source/Utopia/Public/Components/AttributeComponent.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AttributeComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDamageTakenSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UTOPIA_API UAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+
+	UAttributeComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnDamageTakenSignature OnDamageTakenDelegate;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnDeathSignature OnDeahtDelegate;
+
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float DamageAmount);
+
+	UFUNCTION(BlueprintCallable)
+	void RegenHealth(float DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	bool IsAlive();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	float Health = 100.f;
+
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	float MaxHealth = 100.f;
+
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	float HealthRegenRate = 4.f;
+
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	int32 Level = 1;
+
+
+
+public:	
+
+	
+
+		
+};
